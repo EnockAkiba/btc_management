3f17135e52c596f21c84a70ef60a7d1feb4671e0
EnockAkiba <akibawanjumbienock@gmail.com>
2023-10-15 17:35:51 +0200

    update gitugnore file

--- resources/views/livewire/like-livewire.blade.php
+++ resources/views/livewire/like-livewire.blade.php
@@ -1,7 +1,4 @@
 <div class="bg-danger">
-
-<form wire:submit.prevent="like">
     <button class="btn " type="submit" wire:click="like(3)"> <i class="fa fa-heart" aria-hidden="true"></i> </button>
-</form>
 </div>
 <!-- tu remplace 3 par l'id de news -->
